665c8dbfda88e1f853b9b1297fdc87cd1377ce7f
geol2 <big9401@gmail.com>
2023-03-15 18:14:01 +0900

    AppConfig 리팩토링

--- src/main/java/com/meme/code/AppConfig.java
+++ src/main/java/com/meme/code/AppConfig.java
@@ -10,12 +10,18 @@ import com.meme.order.OrderServiceImpl;
 public class AppConfig {
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(getMemberRepository());
+  }
+
+  private FixDiscountPolicy getDiscountPolicy() {
+    return new FixDiscountPolicy();
+  }
+
+  private MemoryMemberRepository getMemberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(
-        new MemoryMemberRepository()
-        , new FixDiscountPolicy() );
+    return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
   }
 }
